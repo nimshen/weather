131b47a19b22adaa9e56a5476e00962870acf4dc
deepHash <itsesisx@gmail.com>
2017-09-12 19:21:36 +0300

    added interface method tester

---
+++ WeatherServiceTests/OpenWeatherMapTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherService.Tests
+{
+    [TestClass()]
+    public class OpenWeatherMapTests
+    {
+        [TestMethod()]
+        public void GetWeatherDataTest()
+        {
+            Location location = new Location("aaaaaaaaaaaaaaaaaaa");
+            IWeatherDataService service = OpenWeatherMap.Instance;
+            Assert.AreNotEqual(null, service.GetWeatherData(location));
+        }
+    }
+}
